591c70cf6b96ed588892dac8c4692b5d989f6b45
Bagus Rizki Setiawan <bagus30122002@gmail.com>
2024-06-14 19:54:58 +0700

    up fitur add note

--- App.js
+++ App.js
@@ -1,33 +1,63 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import CustomButton from "./src/components/CustomButton";
-import CustomTextInput from "./src/components/CustomTextInput";
+import React, { useState } from "react";
+import Home from "./src/screens/home";
+import AddNote from "./src/screens/addNote";
+import EditNote from "./src/screens/editNote";
+import { SafeAreaView, StatusBar } from "react-native";
 
-const App = () => (
-  <View style={styles.container}>
-    <CustomButton
-      backgroundColor="#DDDDDD"
-      color="#39494F"
-      text="Custom Button"
-      width="100%"
-      onPress={() => {}}
-    />
-    <CustomTextInput
-      label="Custom Text"
-      multiline
-      numberOfLines={2}
-      onChange={() => {}}
-    />
-  </View>
-);
+const CurrentPageWidget = ({
+  currentPage,
+  noteList,
+  setCurrentPage,
+  addNote,
+}) => {
+  switch (currentPage) {
+    case "home":
+      return <Home noteList={noteList} setCurrentPage={setCurrentPage} />;
+    case "add":
+      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />;
+    case "edit":
+      return <EditNote />;
+    default:
+      return <Home />;
+  }
+};
 
-const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    padding: 40,
-  },
-});
+const App = () => {
+  const [currentPage, setCurrentPage] = useState("home");
+
+  const [noteList, setNoteList] = useState([
+    {
+      id: 1,
+      title: "Note pertama",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+    },
+  ]);
+
+  // menambahkan Note
+  const addNote = (title, desc) => {
+    const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1;
+
+    setNoteList([
+      ...noteList,
+      {
+        id,
+        title: title,
+        desc: desc,
+      },
+    ]);
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" />
+      <CurrentPageWidget
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        noteList={noteList}
+        addNote={addNote}
+      />
+    </SafeAreaView>
+  );
+};
 
 export default App;
